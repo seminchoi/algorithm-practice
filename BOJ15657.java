5cb4f6e27a1d71509a5a5faf9f2f2147f373304a
guitarcode <choicco89@gmail.com>
2022-03-18 22:58:21 +0900

    Create BOJ15657.java
    [15657번][N과 M] N과 M (8)

---
+++ BOJ15657.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class BOJ15657 {
+	
+	static int N, M;
+	static int[] arr, bf;
+	
+	static void NM(int s, int cur) {
+		if( cur == M - 1 ) {
+			for(int i = s; i < N; i++) {
+				bf[cur] = arr[i];
+				for(int j : bf) {
+					System.out.print(j + " ");
+				}
+				System.out.println("");
+			}
+			return;
+		}
+		for(int i = s; i < N ; i++) {
+			bf[cur] = arr[i];
+			NM(i,cur+1);
+			}		
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		N = sc.nextInt();
+		M = sc.nextInt();
+		arr = new int[N];
+		bf = new int[M]; 
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+	
+		for(int i = 0; i < N ; i++) {
+			bf[0] = arr[i];
+			if(M == 1) System.out.println(bf[0]);
+			else NM(i,1);
+		}
+		
+
+	}
+}
