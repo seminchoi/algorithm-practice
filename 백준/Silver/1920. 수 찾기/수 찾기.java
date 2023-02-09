e9780b0c9fe4740b2a1575c0893d092da1345ec1
Semin Choi <89959944+guitarcode@users.noreply.github.com>
2023-02-09 23:12:19 +0900

    [Silver IV] Title: 수 찾기, Time: 600 ms, Memory: 43952 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,23 +9,42 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
-        Set<Integer> set = new HashSet<>(); 
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            set.add(Integer.parseInt(st.nextToken()));
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        
+        Arrays.sort(arr);
+        
         int m = Integer.parseInt(br.readLine());
         st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             int t = Integer.parseInt(st.nextToken());
-            
-            if(set.contains(t)){
-                sb.append("1\n");
+            int min = 0;
+            int max = n;
+            int mid;
+            while(true){
+                if(min >= max){
+                    sb.append("0\n");
+                    break;
+                }
+                mid = (min + max) / 2;
+
+                if(arr[mid] == t){
+                    sb.append("1\n");
+                    break;
+                }
+
+                else if(arr[mid] < t){
+                    min = mid + 1;
+                }
+
+                else{
+                    max = mid;
+                }
             }
-            else
-                sb.append("0\n");
         }
         System.out.println(sb);
     }
