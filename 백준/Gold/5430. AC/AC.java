5a8b6883c39caa79004b2675b061a456b891384e
Semin Choi <89959944+guitarcode@users.noreply.github.com>
2023-03-06 19:10:03 +0900

    [Gold V] Title: AC, Time: 2648 ms, Memory: 92756 KB -BaekjoonHub

--- 백준/Gold/5430. AC/AC.java
+++ 백준/Gold/5430. AC/AC.java
@@ -1,7 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -16,44 +16,47 @@ public class Main {
             String s = br.readLine();
             int n = Integer.parseInt(br.readLine());
             String arrString = br.readLine();
-            List<Integer> arr = new LinkedList<>();
-            if(n > 0){
+            List<Integer> arr = new ArrayList<>();
+            if (n > 0) {
                 arr = Arrays.stream(arrString.substring(1, arrString.length() - 1).split(",")).map(Integer::parseInt).collect(Collectors.toList());
             }
 
-            long countD = s.chars().filter(c -> c == 'D').count();
-            if(countD > n){
-                sb.append("error\n");
-            }
-            else if(countD == n){
-                sb.append("[]\n");
-            }
-            else {
-                boolean reverse = false;
-                for (int j = 0; j < s.length(); j++) {
-                    char c = s.charAt(j);
-                    if (c == 'R') {
-                        reverse = !reverse;
-                    } else {
-                        arr.remove(reverse ? arr.size()-1 : 0);
+            boolean reverse = false, error = false;
+            for (int j = 0; j < s.length(); j++) {
+                char c = s.charAt(j);
+                if (c == 'R') {
+                    reverse = !reverse;
+                } else {
+                    if (arr.isEmpty()) {
+                        sb.append("error\n");
+                        error = true;
+                        break;
                     }
+                    arr.remove(reverse ? arr.size() - 1 : 0);
                 }
-                sb.append("[");
-                if(reverse){
-                    for (int j = arr.size()-1; j > 0; j--) {
-                        sb.append(arr.get(j)).append(",");
-                    }
-                    sb.append(arr.get(0)).append("]");
+            }
+            if (!error) {
+                if(arr.isEmpty()){
+                    sb.append("[]\n");
                 }
                 else {
-                    for (int j = 0; j < arr.size()-1; j++) {
-                        sb.append(arr.get(j)).append(",");
+                    sb.append("[");
+                    if (reverse) {
+                        for (int j = arr.size() - 1; j > 0; j--) {
+                            sb.append(arr.get(j)).append(",");
+                        }
+                        sb.append(arr.get(0)).append("]");
+                    } else {
+                        for (int j = 0; j < arr.size() - 1; j++) {
+                            sb.append(arr.get(j)).append(",");
+                        }
+                        sb.append(arr.get(arr.size() - 1)).append("]");
                     }
-                    sb.append(arr.get(arr.size()-1)).append("]");
+                    sb.append("\n");
                 }
-                sb.append("\n");
             }
         }
+
 
         System.out.println(sb);
     }
