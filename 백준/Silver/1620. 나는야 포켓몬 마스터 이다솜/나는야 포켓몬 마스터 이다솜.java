0a1707ea28bbd51d9c890ee0bf935ddd29f2b192
Semin Choi <89959944+guitarcode@users.noreply.github.com>
2023-03-30 15:33:48 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 792 ms, Memory: 102336 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -2,24 +2,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.StringTokenizer;
 
 public class Main {
-
-    static class PocketInfo implements Comparable<PocketInfo>{
-        int idx;
-        String s;
-
-        public PocketInfo(int idx, String s) {
-            this.idx = idx;
-            this.s = s;
-        }
-
-        @Override
-        public int compareTo(PocketInfo p) {
-            return s.compareTo(p.s);
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -27,25 +13,23 @@ public class Main {
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
 
-        String[] arr = new String[n];
-        PocketInfo[] sortedArr = new PocketInfo[n];
-
+        String[] arr = new String[n+1];
+        HashMap<String, Integer> map = new HashMap<>();
         for (int i = 0; i < n; i++) {
-            arr[i] = br.readLine();
-            sortedArr[i] = new PocketInfo(i+1, arr[i]);
+            String s = br.readLine();
+            arr[i+1] =  s;
+            map.put(s, i+1);
         }
-
-        Arrays.sort(sortedArr);
 
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < m; i++) {
             String s = br.readLine();
             if(isInteger(s)){
                 int idx = Integer.parseInt(s);
-                sb.append(arr[idx-1]+"\n");
+                sb.append(arr[idx] + "\n");
             }
             else{
-                sb.append(binSearch(sortedArr, s, 0, n-1)+"\n");
+                sb.append(map.get(s) + "\n");
             }
         }
 
@@ -62,20 +46,4 @@ public class Main {
         }
     }
 
-    public static int binSearch(PocketInfo[] arr, String s, int bottom, int top){
-        int mid = (bottom + top) / 2;
-
-        int comp = s.compareTo(arr[mid].s);
-        if(comp == 0) {
-            return arr[mid].idx;
-        }
-        else if(comp < 0){
-            return binSearch(arr, s, bottom, mid);
-
-        }
-        else{
-            return binSearch(arr, s, mid + 1, top);
-
-        }
-    }
 }
