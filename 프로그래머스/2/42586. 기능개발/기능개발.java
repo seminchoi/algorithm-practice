45153db76b1b2dfabd236e925fa6dcb507e215b1
Semin Choi <89959944+seminchoi@users.noreply.github.com>
2024-03-25 15:10:40 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] progresses, int[] speeds) {
+        int curIndex = 0;
+        List<Integer> deliveryCounts = new ArrayList<>();
+        while(curIndex < progresses.length) {
+            doProgress(progresses, speeds);
+            int nextIndex = checkDeliveryIndex(progresses, curIndex);
+            if(curIndex < nextIndex) {
+                deliveryCounts.add(nextIndex-curIndex);
+                curIndex = nextIndex;
+            }
+        }
+        
+        int[] answer = new int[deliveryCounts.size()];
+        for(int i = 0; i < deliveryCounts.size(); i++) {
+            answer[i] = deliveryCounts.get(i);
+        }
+        return answer;
+    }
+    
+    private void doProgress(int[] progresses, int[] speeds) {
+        int length = progresses.length;
+        for(int i = 0; i < length; i++) {
+            progresses[i] += speeds[i];
+            progresses[i] = progresses[i] >= 100 ? 100 : progresses[i];
+        }
+    }
+    
+    private int checkDeliveryIndex(int[] progresses, int curIndex) {
+        while(curIndex < progresses.length && progresses[curIndex] >= 100) {
+            curIndex++;
+        }
+        return curIndex;
+    }
+}
