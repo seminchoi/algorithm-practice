1dbea3ef05b084db8b9e81a882ed9af635dc1347
Semin Choi <89959944+seminchoi@users.noreply.github.com>
2024-05-22 22:21:27 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 296 ms, Memory: 20992 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+import java.util.Arrays;
+public class Main {
+ 
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] P = new int[1001];
+		int[] DP = new int[1001];
+		
+		
+		for(int i = 1; i <= N; i++) {
+			P[i] = sc.nextInt();
+		}
+		
+		Arrays.fill(DP, 0);
+		
+		for(int i = 1; i <= N; i++) {
+			DP[i] = P[i];
+			for(int j = 1; j < i; j++) {
+				if(DP[i] > DP[i-j] + P[j])
+					DP[i] = DP[i-j] + P[j];
+			}
+		}
+		System.out.println(DP[N]);
+	}
+
+}
